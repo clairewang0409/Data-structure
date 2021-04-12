a881f1bb0aba61d71631b18d12885e5839befd29
clairewang0409 <70304861+clairewang0409@users.noreply.github.com>
2021-04-12 11:57:27 +0800

    Update Merge K Sorted Sequences.py

--- assignment1/Merge K Sorted Sequences.py
+++ assignment1/Merge K Sorted Sequences.py
@@ -8,3 +8,31 @@ def MergeKSortedSequences(sequences):
     arr.reverse()
     #print(arr)
     return arr
+
+
+
+'''
+def MergeTwoSortedList(ListA, ListB):
+    """
+    A simple function that merges two sorted lists(increase) into one sorted list(increase)
+    ListA and ListB must be a sorted list !!!
+    """
+    MergeList = []
+    i = 0
+    j = 0
+    while( i < len(ListA) and j < len(ListB)):
+        if ListA[i] < ListB[j]:
+            MergeList.append(ListA[i])
+            i += 1
+        else:
+            MergeList.append(ListB[j])
+            j += 1
+            
+    if i < len(ListA):
+        MergeList.extend(ListA[i:])
+        
+    if j < len(ListB):
+        MergeList.extend(ListB[j:])
+
+    return(MergeList)
+'''
